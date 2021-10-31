ffddc6dac8824d036de07b4b9e312354e9bbbb5a
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-10-31 12:06:53 +0100

    managed to display the year in he terminal

--- index.js
+++ index.js
@@ -3,16 +3,18 @@
 
 // test line
 //console.log("Hello, Node.JS!");
+
+//node packages
 const axios = require('axios');
 const chalk = require('chalk');
 
 //make color in terminal
-console.log(chalk.blue('Hello world!'));
+console.log(chalk.magenta('Join the dark side, we have cookies!!'));
 
 //Stylize the terminal
 var figlet = require('figlet');
 
-figlet('Hello World!!', function(err, data) {
+figlet('Unicorn World', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
@@ -22,4 +24,11 @@ figlet('Hello World!!', function(err, data) {
 });
 
 
+//Start code here
+const api ="https://date.nager.at/api/v3/PublicHolidays/2017/AT";
+console.log(api);
 
+//Get the year (4 digits)
+const day = new Date();
+let year = day.getFullYear();
+console.log(year);
